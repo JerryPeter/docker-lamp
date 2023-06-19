efd5eb16974b441f830f7443b630caf8313d6015
Jerry Peter <jerry.peter@modena.com>
2023-06-19 13:13:39 +0700

    Initial Commit

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<?php
+    echo "Hello NGINX <br>";
+
+    $servername = "mysql";
+    $username = "root";
+    $password = "secret";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";   
+
+    phpinfo();
+?>
