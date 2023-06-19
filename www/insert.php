64f4ca35b6511af4378f053e8e0f40f56067e071
Jerry Peter <jerry.peter@modena.com>
2023-06-19 17:16:37 +0700

    Initial Commit

---
+++ www/insert.php
@@ -0,0 +1,28 @@
+<?php
+    $servername = "mysql";
+    $username = "root";
+    $password = "secret";
+    $dbname = "hello";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO test (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com')";
+    
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully <br>";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
+    }
+    
+    $conn->close();  
+
+    echo "Connected successfully";   
+
+?>
